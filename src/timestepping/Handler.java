e418881ff5efd48b14ef7cf7066a040764ce1078
Madolph <Iorael@web.de>
2017-10-26 12:40:41 +0200

    replaced the Overlord

--- src/timestepping/Handler.java
+++ src/timestepping/Handler.java
@@ -13,6 +13,8 @@ import clearcl.backend.ClearCLBackendInterface;
 import clearcl.backend.ClearCLBackends;
 import clearcl.enums.ImageChannelDataType;
 import clearcl.viewer.ClearCLImageViewer;
+import simbryo.synthoscopy.microscope.lightsheet.drosophila.LightSheetMicroscopeSimulatorDrosophila;
+import simbryo.synthoscopy.microscope.parameters.IlluminationParameter;
 
 public class Handler {
 
@@ -32,8 +34,6 @@ public class Handler {
 	 * The TimeStepper used for the Test
 	 */
 	public TimeStepper mTimeStepper;
-	
-	public Plotter mPlotter;
 	
 	/**
 	 * duration of the Test in seconds
@@ -78,14 +78,6 @@ public class Handler {
 			mPred = new PredictorHoltWinters();
 		
 		mTimeStepper = new TimeStepper(0.5f, 0.2f, 1f, 0.1f);	
-		
-		mProgram1 = mContext.createProgram(Handler.class, "Calculator.cl");
-		mProgram1.addDefine("CONSTANT", "1");
-		mProgram1.buildAndLog();
-		
-		mProgram2 = mContext.createProgram(Handler.class, "Simulator.cl");
-		mProgram2.addDefine("CONSTANT", "1");
-		mProgram2.buildAndLog();
 	}
 	
 	@Test
@@ -95,8 +87,16 @@ public class Handler {
 		
 		InitializeModules(StDev);
 		
-		mPlotter = new Plotter();
-		mPlotter.initializePlotSimpleSimStepper(mFxOn);
+		mProgram1 = mContext.createProgram(Handler.class, "Calculator.cl");
+		mProgram1.addDefine("CONSTANT", "1");
+		mProgram1.buildAndLog();
+		
+		mProgram2 = mContext.createProgram(Handler.class, "Simulator.cl");
+		mProgram2.addDefine("CONSTANT", "1");
+		mProgram2.buildAndLog();
+		
+		Plotter Plotter = new Plotter();
+		Plotter.initializePlotSimpleSimStepper(mFxOn);
 		
 		int lSize = 128;
 		ClearCLImage lImage = mContext.createSingleChannelImage(ImageChannelDataType.Float, lSize, lSize, lSize);
@@ -125,7 +125,7 @@ public class Handler {
 				}
 				float step = mTimeStepper.computeNextStep(metric);  
 				
-				mPlotter.plotDataSimpleSimStepper(time,
+				Plotter.plotDataSimpleSimStepper(time,
 						step/100,
 						mPred.prediction,
 						mPred.average);
@@ -136,6 +136,98 @@ public class Handler {
 		lViewImage.waitWhileShowing();
 	}
 	
+	@Test
+	public void SimbryoTest() throws Exception
+	{
+		boolean StDev = true;
+		
+		InitializeModules(StDev);
+		
+		mProgram1 = mContext.createProgram(Simulator.class, "Calculator.cl");
+		mProgram1.addDefine("CONSTANT", "1");
+		mProgram1.buildAndLog();
+			  
+		// now that this is done, we initialize the time and create two images that will
+		// be filled by the simulator during the run
+		float time=0;
+			  
+		int lSize = 128;
+			  
+		int lPhantomWidth = lSize;
+		int lPhantomHeight = lPhantomWidth;
+		int lPhantomDepth = lPhantomWidth;
+		      
+		//hier drigend auf Datentyp achten
+		ClearCLImage lImage = mContext.createSingleChannelImage(ImageChannelDataType.UnsignedInt16, 
+																lSize, lSize, lSize);
+			  
+		ClearCLImageViewer lViewImage = ClearCLImageViewer.view(lImage);
+			  
+		Plotter Graph = new Plotter();
+		Graph.initializePlot(mFxOn); //mFxOn will be checked and set to true here if it is not already true
+			  
+		int lNumberOfDetectionArms = 1;
+		int lNumberOfIlluminationArms = 4;
+		int lMaxCameraResolution = lSize;
+			  
+		LightSheetMicroscopeSimulatorDrosophila lSimulator =
+                      new LightSheetMicroscopeSimulatorDrosophila(mContext,
+                                                             		lNumberOfDetectionArms,
+                                                             		lNumberOfIlluminationArms,
+                                                             		lMaxCameraResolution,
+                                                             		5f,
+                                                             		lPhantomWidth,
+                                                             		lPhantomHeight,
+                                                             		lPhantomDepth);
+			  
+		@SuppressWarnings("unused")
+		ClearCLImageViewer lCameraImageViewer =
+                      lSimulator.openViewerForCameraImage(0);
+			  
+			  // as long as we aren't above the time, we will now generate pictures and compute timesteps from them
+		while (time<(mDuration*1000))  
+		{
+			float currStep = mTimeStepper.mStep;
+			System.out.println("current time is: "+time);
+
+			lSimulator.simulationSteps((int)currStep/10);
+				  
+		lSimulator.setNumberParameter(IlluminationParameter.Height,
+                          					0,
+                          					1f);
+			lSimulator.setNumberParameter(IlluminationParameter.Intensity,
+                          					0,
+                          					50f);
+			lSimulator.setNumberParameter(IlluminationParameter.Gamma,
+                          					0,
+                          					0f);
+
+		    lSimulator.render(true);
+		          
+		    lSimulator.getCameraImage(0).copyTo(lImage, true);
+		    lImage.notifyListenersOfChange(mContext.getDefaultQueue());
+			mCalc.CachePic(lImage, mContext, lSize);
+				  
+			Thread.sleep((long) currStep);
+			if (mCalc.filled)
+			{			  
+				// computes the difference between the two pictures
+				float diff = mCalc.compareImages(mProgram1, lSize);
+				//System.out.println("diff is: "+diff);
+				// computed the step out of the saved difference
+				float step = mTimeStepper.computeStep(diff, time);
+			  
+				// put the Thread to sleep to simulate realtime... kinda... sorta
+			  
+				//System.out.println("computed step is: "+step);
+					  
+				Graph.plotdata(time, mTimeStepper.mInfo.mDev[0][0], mTimeStepper.mInfo.mDev[0][0]/*+mTimeStepper.mInfo.mCurrentSigma*/, step, mTimeStepper.mInfo.mMean.val);
+			}
+			time += currStep;
+		}	  
+		lViewImage.waitWhileShowing();
+		lSimulator.close();
+	}
 	
 	@Test
 	public void PredictorDemo()
