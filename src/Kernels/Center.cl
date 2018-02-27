1e8e07361f5492942388f4f9fe8ede0ec68cca02
Madolph <Iorael@web.de>
2018-02-27 16:47:24 +0100

    added Center Detection Functionalities

--- src/Kernels/Center.cl
+++ src/Kernels/Center.cl
@@ -15,8 +15,77 @@
 // (check method: myprogram.getSourceCode())
 #include "blu/tada.cl"	
 
+
 __kernel
-void Sum3D (__read_only image3d_t image,
+void ScanPlaneX (__read_only image3d_t image,
+            __global    float*    arrayX,
+            int planeDimY,
+            int planeDimZ) 
+{
+  const int x       = get_global_id(0);
+  const int y       = get_global_id(1);
+  const int z       = get_global_id(2);
+  
+  const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP | CLK_FILTER_NEAREST;
+  
+  float sum = 0;
+  
+  const int4 origin = (int4){x,y,z};
+  
+  for(int ly=0; ly<planeDimX; ly++)
+  {
+    for(int lz=0; lz<planeDimY; lz++)
+    {
+      const int4 pos = origin + (int4){0,ly,lz,0};
+     
+      float value = read_imagef(image, sampler, pos).x;
+
+      sum = sum + value;
+      }
+    }
+  }
+  
+  const int index = x;
+  
+  arrayX[index] = sum;
+}
+
+__kernel
+void ScanPlaneY (__read_only image3d_t image,
+            __global    float*    arrayY,
+            int planeDimX,
+            int planeDimZ) 
+{
+  const int x       = get_global_id(0);
+  const int y       = get_global_id(1);
+  const int z       = get_global_id(2);
+  
+  const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP | CLK_FILTER_NEAREST;
+  
+  float sum = 0;
+  
+  const int4 origin = (int4){x,y,z};
+  
+  for(int lx=0; lx<planeDimX; lx++)
+  {
+    for(int lz=0; lz<planeDimZ; lz++)
+    {
+      const int4 pos = origin + (int4){lx,0,lz,0};
+     
+      float value = read_imagef(image, sampler, pos).x;
+
+      sum = sum + value;
+      }
+    }
+  }
+  
+  const int index = x;
+  
+  arrayX[index] = sum;
+}
+
+__kernel
+void ScanPlaneZ (__read_only image3d_t image,
             __global    float*    arrayZ,
             int planeDimX,
             int planeDimY) 
@@ -31,11 +100,11 @@ void Sum3D (__read_only image3d_t image,
   
   const int4 origin = (int4){x,y,z};
   
-  for(int ly=0; ly<planeDimX; ly++)
+  for(int lx=0; lx<planeDimX; lx++)
   {
-    for(int lx=0; lx<planeDimY; lx++)
+    for(int ly=0; ly<planeDimY; ly++)
     {
-      const int4 pos = origin + (int4){lx,ly,lz,0};
+      const int4 pos = origin + (int4){lx,ly,0,0};
      
       float value = read_imagef(image, sampler, pos).x;
 
@@ -48,3 +117,4 @@ void Sum3D (__read_only image3d_t image,
   
   arrayZ[index] = sum;
 }
+
---
+++ src/toolset/CenterDetector.java
@@ -0,0 +1,117 @@
+package toolset;
+
+import java.io.IOException;
+
+import Kernels.KernelDemo;
+import clearcl.ClearCLBuffer;
+import clearcl.ClearCLContext;
+import clearcl.ClearCLImage;
+import clearcl.ClearCLKernel;
+import clearcl.ClearCLProgram;
+import coremem.enums.NativeTypeEnum;
+import coremem.offheap.OffHeapMemory;
+
+public class CenterDetector {
+	
+	ClearCLContext mContext;
+	
+	ClearCLKernel mScanX, mScanY, mScanZ;
+	
+	ClearCLProgram mScanPlane;
+	
+	public ClearCLBuffer mArrayX, mArrayY, mArrayZ;
+
+	public CenterDetector(ClearCLContext context) throws IOException
+	{
+		mContext = context;
+		mScanPlane = mContext.createProgram(KernelDemo.class, "Center.cl");
+		mScanX = mScanPlane.createKernel("ScanPlaneX");
+		mScanY = mScanPlane.createKernel("ScanPlaneY");
+		mScanZ = mScanPlane.createKernel("ScanPlaneZ");
+	}
+	
+	public float[] detectCenter(ClearCLImage image)
+	{
+		float[] center = new float[3];
+		
+		center[0]=getCenterX(image);
+		center[1]=getCenterY(image);
+		center[2]=getCenterZ(image);
+		
+		return center;
+	}
+	
+	public float getCenter(ClearCLBuffer array)
+	{
+		float position = 0;
+		
+		OffHeapMemory lBuffer = OffHeapMemory.allocateFloats(array.getLength());
+		array.writeTo(lBuffer, true);
+		
+	    float totalWeight = 0;
+	    float coordWeight = 0;
+	    for (int i = 0; i < array.getLength(); i++)
+	    { 
+	    	totalWeight += lBuffer.getFloatAligned(i);
+	    	coordWeight += (lBuffer.getFloatAligned(i)*i);
+	    }
+	    position= coordWeight/totalWeight;
+		
+		return position;
+	}
+	
+	public float getCenterX(ClearCLImage image)
+	{
+		long [] imageDim = image.getDimensions();
+		
+		mArrayX = mContext.createBuffer(NativeTypeEnum.Float, imageDim[0]);
+		
+		mScanX.setArgument("image", image);
+		mScanX.setArgument("arrayX", mArrayX);
+		mScanX.setArgument("planeDimY", imageDim[1]);
+		mScanX.setArgument("planeDimZ", imageDim[2]);
+		mScanX.setGlobalSizes(imageDim[0],1,1);
+		mScanX.run(true);
+		
+		float center = getCenter(mArrayX);
+		
+		return center;
+	}
+	
+	public float getCenterY(ClearCLImage image)
+	{
+		long [] imageDim = image.getDimensions();
+		
+		mArrayY = mContext.createBuffer(NativeTypeEnum.Float, imageDim[1]);
+		
+		mScanY.setArgument("image", image);
+		mScanY.setArgument("arrayX", mArrayX);
+		mScanY.setArgument("planeDimX", imageDim[0]);
+		mScanY.setArgument("planeDimZ", imageDim[2]);
+		mScanY.setGlobalSizes(1,imageDim[1],1);
+		mScanY.run(true);
+		
+		float center = getCenter(mArrayY);
+		
+		return center;
+	}
+	
+	public float getCenterZ(ClearCLImage image)
+	{
+		long [] imageDim = image.getDimensions();
+		
+		mArrayZ = mContext.createBuffer(NativeTypeEnum.Float, imageDim[2]);
+		
+		mScanZ.setArgument("image", image);
+		mScanZ.setArgument("arrayX", mArrayX);
+		mScanZ.setArgument("planeDimX", imageDim[0]);
+		mScanZ.setArgument("planeDimY", imageDim[1]);
+		mScanZ.setGlobalSizes(1,1,imageDim[2]);
+		mScanZ.run(true);
+		
+		float center = getCenter(mArrayZ);
+		
+		return center;
+	}
+	
+}
